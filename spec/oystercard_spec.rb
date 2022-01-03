d6f3fc88e3ab69766c6f72caf53618781ccb8120
Tom Mayor <thomasmayor89@gmail.com>
2022-01-03 15:08:50 +0000

    04 added default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
 require "oystercard"
 
 describe Oystercard do 
-  
+
+  describe "#balance" do
+
+    it "has a balance of zero" do
+      expect(subject.balance).to eq(0)
+    end
+    
+  end
+
 end
